80568faa3c60015f4948bdca4507cc6bfd661340
VWFerreira <98263784+VWFerreira@users.noreply.github.com>
2025-04-01 23:25:32 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,14 +3,14 @@ import json
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
-# Carregar as credenciais do Google armazenadas na variável de ambiente
+# Acessar as credenciais armazenadas na variável de ambiente do GitHub
 google_credentials_json = os.getenv('GOOGLE_CREDENTIALS')
 
 # Verificar se a variável de ambiente está definida corretamente
 if google_credentials_json is None:
     raise ValueError("A variável GOOGLE_CREDENTIALS não está definida!")
 
-# Carregar as credenciais do serviço a partir da string JSON
+# Carregar as credenciais do Google a partir da string JSON
 google_credentials = json.loads(google_credentials_json)
 
 # Agora você pode usar as credenciais para acessar a API do Google
@@ -24,4 +24,5 @@ SPREADSHEET_ID = '1gSBcV5EPYYO4mIMh7yNqNs9-GshaR-jR'
 RANGE_NAME = 'Sheet1!A1:H'
 
 result = sheets_service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+
 
